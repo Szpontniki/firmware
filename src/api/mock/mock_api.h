1ca0f378aecf6d07f0b3f828d9f02aa0a77e1c85
Khenzii <khenzii@khenzii.dev>
2026-09-20 04:00:33 +0200

    fix(api): resolve CORS issues

--- src/api/mock/mock_api.h
+++ src/api/mock/mock_api.h
@@ -4,9 +4,21 @@
 #include "../api.h"
 #include "../../display/tui/tui_display.h"
 
+struct CORSMiddleware {
+	struct context {};
+
+	void before_handle(crow::request&, crow::response&, context&) {}
+
+	void after_handle(crow::request&, crow::response &res, context&) {
+		res.set_header("Access-Control-Allow-Origin", "*");
+		res.set_header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+		res.set_header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+	}
+};
+
 class MockAPI : public API {
 	private:
-		crow::SimpleApp app;
+		crow::App<CORSMiddleware> app;
 	public:
 		MockAPI(IDisplay *display);
 		void start() override;
